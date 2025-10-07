5498726a15f2a948c68d0281a75e87af1dcb59af
AkaashParmar <akash@netfotech.in>
2025-10-07 21:53:05 +0530

    setup

--- config/db.js
+++ config/db.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('MongoDB connected');
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
   }
 };
