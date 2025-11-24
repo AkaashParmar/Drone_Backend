5fd4effa90ab48de68292007d16ca8725c0c5f05
Akaashparmar <akash@netfotech.in>
2025-11-24 11:05:09 +0530

    oregin

--- server.js
+++ server.js
@@ -34,11 +34,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173" , "https://drone-frontend-beryl.vercel.app"],
   credentials: true, 
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
 
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
